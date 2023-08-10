7078285ae3f32bb21e23001103d5d9066f34bca2
Rensytimsy <rensytimsy03@gmail.com>
2023-08-10 14:26:13 -0700

    again code in assembly

--- 0x00-hello_world/100-main.c
+++ 0x00-hello_world/100-main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *main - entry point
+ *Description: "printing code in assembly code"
+ *Return: 0 (Success)
+ */
 int main(void)
 {
 printf("Holberton School");
-return(0);
+return (0);
 }
