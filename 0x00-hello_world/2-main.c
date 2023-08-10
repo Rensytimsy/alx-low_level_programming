447076d21967ab47dc99301cf876d2789de8e66b
Rensytimsy <rensytimsy03@gmail.com>
2023-08-10 06:49:51 -0700

    in assembler code

---
+++ 0x00-hello_world/2-main.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(void){
+  printf("holberton school");
+  return(0);
+}
--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,4 +1,8 @@
-	.file	"main.c"
+	.file	"2-main.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"hello world"
 	.text
 	.globl	main
 	.type	main, @function
@@ -11,6 +15,9 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
 	movl	$0, %eax
 	popq	%rbp
 	.cfi_def_cfa 7, 8
