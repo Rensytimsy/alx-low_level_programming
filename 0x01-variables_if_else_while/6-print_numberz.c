29a1788d4430c358c6d829d3fd49566a474b9817
Rensytimsy <rensytimsy03@gmail.com>
2023-08-11 15:42:34 -0700

    numbers in char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - starting point of the program
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
