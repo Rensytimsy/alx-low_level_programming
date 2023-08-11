691e27f5e5227f30adb506cc06a223ccc196c961
Rensytimsy <rensytimsy03@gmail.com>
2023-08-11 16:01:39 -0700

    alphabets in revrse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main is the starting point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char l;
+for (l = 'z'; l >= 'a'; l--)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
