97a8c87c3a53da547dbcbbab9f1526c7bf1a81c9
Rensytimsy <rensytimsy03@gmail.com>
2023-08-11 15:30:00 -0700

    printing numbers at the base of 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - is the starting point of the program
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+int nums;
+for (nums = 0; nums < 9; nums++)
+{
+printf("%d", nums);
+}
+printf("%d\n", nums);
+return (0);
+}
