e3647e97869875ff04f700c761cc059ac79fdd2e
Rensytimsy <rensytimsy03@gmail.com>
2023-08-12 12:14:19 -0700

    last line print character sequence escape

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>  // Include this header for STDOUT_FILENO
+#include <unistd.h>  
 
 /**
  * main - Entry point
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-    write(STDOUT_FILENO, "and that piece of art is useful"
-                           " - Dora Korpar, 2015-10-19\n", 59);
+    write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
     return (1);
 }
