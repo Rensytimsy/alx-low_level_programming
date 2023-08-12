e55867a89a42c33ada2bbc7c44c945bb37d97647
Rensytimsy <rensytimsy03@gmail.com>
2023-08-12 11:33:04 -0700

    The sassembly intel

--- 0x00-hello_world/100-main.c
+++ 0x00-hello_world/100-main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/**
- *main - entry point
- *Description: "printing code in assembly code"
- *Return: 0 (Success)
- */
-int main(void)
-{
-printf("Holberton School");
-return (0);
+int main(void){
+    printf("Holberton School");
+    return(0);
 }
+
