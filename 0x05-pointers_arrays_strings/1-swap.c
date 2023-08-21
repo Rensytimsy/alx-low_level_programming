ab5ba5da5c928bf167ca8dcb8e4029e3fe8324f5
Rensytimsy <rensytimsy03@gmail.com>
2023-08-21 09:15:26 -0700

    1. Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * swap_int - is a void function
+ * @a : is an int type variable
+ * @b : is an int type variable
+ */
 void swap_int(int *a, int *b)
 {
 int num;
 num = *a;
 *a = *b;
-*b = num; 
+*b = num;
 }
