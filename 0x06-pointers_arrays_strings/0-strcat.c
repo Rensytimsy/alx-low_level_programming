6e37f2affeb838186dbf354e3ece4bb28e10ba64
Rensytimsy <rensytimsy03@gmail.com>
2023-08-24 00:30:12 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _strcat - is our main function
+ * @dest: a string parameter.
+ * @src:a string parameter.
+ * Return: (0) Success.
+ */
 char *_strcat(char *dest, char *src)
 {
-  int i,j;
-  for(i = 0; dest[i] != '\0'; i++);
-  for(j = 0; src[j] != '\0'; j++)
-  {
-    dest[i] = src[j];
-    i++;
-  }
-  dest[i]='\0';
-  printf("%s\n", dest);
-  return 0;
+int i, j;
+for (i = 0; dest[i] != '\0'; i++);
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+dest[i] = '\0';
+printf("%s\n", dest);
+return (0);
 }
