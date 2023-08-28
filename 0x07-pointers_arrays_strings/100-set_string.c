f10b72b56b01d78d4b7774701c139b919d937145
Rensytimsy <rensytimsy03@gmail.com>
2023-08-28 10:59:43 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - is our main function.
+ *@s: is a pointer to another pointer
+ *@to: is a param passed.
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
