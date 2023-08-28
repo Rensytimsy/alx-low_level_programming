9bd1952b16f86d4140f73eb821a1518cb3533fe3
Rensytimsy <rensytimsy03@gmail.com>
2023-08-28 09:33:23 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-while (*s)
-{
-if (*s != c)
-s++;
-else
-return (s);
-}
-if (*s == '\0')
-return (NULL);
-else
-return (s);
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	if (s[i] == c)
+		return (s + i);
+
+	return (0);
 }
