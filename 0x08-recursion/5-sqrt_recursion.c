733d95f72155a60667724a06c268b53816d81243
Rensytimsy <rensytimsy03@gmail.com>
2023-08-30 12:16:45 -0700

    all

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *_sqrt_recursion - is the main function.
  *sqrt1 - is a subordinate function that helps perform operations
  *@x: is an integer
  *@y: is an integer
@@ -15,7 +14,11 @@ if (y * y >  x)
 return (-1);
 return (sqrt1(x, y + 1));
 }
-
+/**
+ *_sqrt_recursion - is the main function.
+ *@n: an integer
+ *Return: answer
+ */
 int _sqrt_recursion(int n)
 {
 return (sqrt1(n, 1));
