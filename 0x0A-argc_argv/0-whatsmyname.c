1026abf10f0c364ddb2f5950da05d3cd62b60139
Rensytimsy <rensytimsy03@gmail.com>
2023-09-01 11:45:29 -0700

    Whtas my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-int main(int argc __attribute__((unused)), char* argv[])
+/**
+ *main - is our main function
+ *@argc: is the argument count passed in the command line
+ *@argv: is an array of arguments passed in the command line
+ *Return: 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
