7cadf72d06fabfd5c93338fd889422e9a7f1f73d
Rensytimsy <rensytimsy03@gmail.com>
2023-09-11 21:37:58 +0000

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,14 +8,21 @@
 void print_dog(struct dog *d)
 {
 if (d == NULL)
+{
 return;
-
+}
 if (d->name == NULL)
-printf("nil");
+{
+printf("nil\n");
+}
 else if (d->owner == NULL)
-printf("nil");
-
+{
+printf("nil\n");
+}
+else
+{
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
+}
 }
