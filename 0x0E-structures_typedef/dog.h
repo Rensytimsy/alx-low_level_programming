c38fdb2e608e9652849f723a2bd621b1a60f1609
Rensytimsy <rensytimsy03@gmail.com>
2023-09-11 20:49:03 +0000

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog - Dog
+ *@name: is the first element in the struct
+ *@owner: is the second element in the struct
+ *@age: is the third element in the struct
+ */
+
 typedef struct dog
 {
 char *name;
 char *owner;
 float age;
-} dog_one;
-
-/**
- * dog - is a struct
- *@name: is the first element in the struct
- *@owner: is the second element in the struct
- *@age: is the third element in the struct
- */
+} dog_d;
 
 #endif
