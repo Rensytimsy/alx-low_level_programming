bc87791fffd5739a4add3c2f4e0d7430a809823c
Rensytimsy <rensytimsy03@gmail.com>
2023-09-13 14:58:10 +0000

    array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
 #ifndef FUNC_POINT_H
 #define FUNC_POINT_H
 #include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 #endif
