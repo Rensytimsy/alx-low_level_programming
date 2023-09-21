1b8895f29d17188b611bfb27ecce69cb6403222c
Rensytimsy <rensytimsy03@gmail.com>
2023-09-21 22:09:07 +0300

    length list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 /**
- *
+ *list_len - takes a list as an argument
+ *@h: is a new node
+ *Return: l size of node
 */
 
 size_t list_len(const list_t *h)
 {
-  size_t l = 0;
-  while(h)
-  {
-    l++;
-    h = h->next;
-  }
-  return (l);
+size_t l = 0;
+while (h)
+{
+l++;
+h = h->next;
+}
+return (l);
 }
