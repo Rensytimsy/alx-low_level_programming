3d0eb3643dcc2bbc10b5a26a66032ed65c6ec147
Rensytimsy <rensytimsy03@gmail.com>
2023-09-21 20:55:14 +0300

    changed the name of main.h to lists.h

--- 0x12-signly_linked_lists/0-main.c
+++ 0x12-signly_linked_lists/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 /**
  * main - check the code
  *
--- 0x12-signly_linked_lists/0-print_list.c
+++ 0x12-signly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 /**
  * print_list - is the main function and takes one const parameter.
  * @h: is a node passed;
