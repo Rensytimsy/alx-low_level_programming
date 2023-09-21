0c22206fa9612a4eb509cc94bd69cafbb93f46b2
Rensytimsy <rensytimsy03@gmail.com>
2023-09-21 21:04:43 +0300

    print lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - is the main function and takes one const parameter.
+ * @h: is a node passed;
+ * Return: (l);
+*/
+size_t print_list(const list_t *h)
+{
+size_t l = 0;
+/*Should return number of nodes*/
+while (h)
+{
+if (h->str == NULL)
+printf("%s\n", "[0] (nill)");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+l++;
+}
+return (l);
+}
