0c8a7219f53947e26bfcb677882783f0ce4a3ee3
Rensytimsy <rensytimsy03@gmail.com>
2023-09-25 23:23:20 +0300

    delete first

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,17 +6,11 @@
 */
 int pop_listint(listint_t **head)
 {
-/*secNode will take the value of the first node*/
 listint_t *firstNode;
-/*If we reach a the last node which will be null return error*/
 if (*head == NULL)
-return (-1);
-
-/*The code below assigns secNode to the first node*/
+return (0);
 firstNode = *head;
-/*The code below moves head to the next node in line*/
 *head = (*head)->next;
-/*When we use free we are freeing the first node*/
 free(firstNode);
 return (0);
 }
