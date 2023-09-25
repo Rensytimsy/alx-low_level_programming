9516bac81fa51d9dcd29f864883e2b2d41f1f057
Rensytimsy <rensytimsy03@gmail.com>
2023-09-25 17:47:20 +0300

    0. print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *print_listint - main function takes one parameter
+ *@h: is a new node with a value.
+ *Return: values present in the nodes.
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t n = 0;
+while(h)
+{
+printf("%d\n", h->n);
+n++;
+h = h->next;
+}
+return(n);
+}
