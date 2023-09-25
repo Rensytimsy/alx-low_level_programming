134f3784f9bfe479f7e85c78f1610b87b6785759
Rensytimsy <rensytimsy03@gmail.com>
2023-09-25 18:07:18 +0300

    fixed some betty at lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -11,13 +11,12 @@ int _putchar(char c);
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
 int n;
 struct listint_s *next;
-}listint_t;
+} listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 #endif
