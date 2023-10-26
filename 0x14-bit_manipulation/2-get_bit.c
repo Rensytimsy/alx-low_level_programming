309896c0b0a1cd8ace47dcb259af043a22f9181e
Rensytimsy <rensytimsy03@gmail.com>
2023-10-26 15:31:27 +0300

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * get_bit - func takes two args n and index
+ *@n: binary number
+ *@index: binary index/position
+ *Return: bit value
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int k;
+
+if (n == 0 && index < 64)
+return (0);
+for (k = 0; k <= 63; n >>= 1, k++)
+{
+if (index == k)
+{
+return (n & 1);
+}
+}
+return (-1);
+}
