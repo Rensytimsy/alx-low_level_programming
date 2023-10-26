72eed2a590c180de005218eb7fde8952a83e6b69
Rensytimsy <rensytimsy03@gmail.com>
2023-10-26 16:04:48 +0300

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *flip_bits - function takes two args n and m
+ *@n: bit number
+ *@m: number
+ *Return: number of bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned int num_of_bits;
+for (num_of_bits = 0; n || m; n >>= 1, m >>= 1)
+{
+if ((n & 1) != (m & 1))
+num_of_bits++;
+}
+
+return (num_of_bits);
+}
