1e2b228391049a110c687af5f89918806b12cf9c
Rensytimsy <rensytimsy03@gmail.com>
2023-10-30 16:12:00 +0300

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *create_file - function takes two arguments filename and text_content
+ *@filename: The name of a file
+ *@text_content: text present in a file
+ *Return: 1
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, size, i;
+char *buffer;
+
+if (filename == NULL)
+return (-1);
+if (text_content == NULL)
+{
+fd = open(filename, O_CREAT, 0600);
+if (fd == -1)
+return (-1);
+return (1);
+}
+for (i = 0; text_content[i] != '\0'; i++)
+buffer = malloc(i * sizeof(char));
+if (buffer == NULL)
+return (-1);
+fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+if (fd == -1)
+return (-1);
+size = write(fd, text_content, i);
+if (size == -1)
+return (-1);
+close(fd);
+free(buffer);
+return (1);
+}
